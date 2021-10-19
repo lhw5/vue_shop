c45010dcc3b71744245c562bf5bb7929e232d55f
lhw2435 <lhw2435@qq.com>
2021-10-19 10:32:59 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+import './assets/css/global.css'
+import axios from 'axios'
 Vue.config.productionTip = false
-
+Vue.prototype.$http = axios
+//配置请求根路径
+axios.defaults.baseURL = 'https://api.naccl.top/vue/shop/api/private/v1/'
 new Vue({
   router,
   render: h => h(App)
